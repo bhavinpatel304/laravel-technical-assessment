5158d3feb2c9590fb2e0c585faa470c806b1e89b
Bhavin Patel <bhavin.ce304@gmail.com>
2025-04-21 23:26:31 -0600

    peopel view

--- resources/views/livewire/people.blade.php
+++ resources/views/livewire/people.blade.php
@@ -5,7 +5,7 @@
         <input
         type="text"
         wire:model.debounce.300ms="search"
-        placeholder="Search people from star wars..."
+        placeholder="Search people"
         class="border border-gray-300 rounded-lg px-4 py-2 w-1/2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <button
